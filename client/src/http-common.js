457f53e16269570faa681a433c506027fddec5b3
Dangvhh <vohuynhhaidang24032001@gmail.com>
2022-10-20 17:10:40 +0700

    change baseUrl axios

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://safe-refuge-59222.herokuapp.com/api",
   headers: {'X-Requested-With': 'XMLHttpRequest'}
 });
